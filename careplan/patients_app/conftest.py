4935784c480a1511e3384e8d094249886ef672d5
szymon.zietek721@gmail.com <szymon.zietek721@gmail.com>
2023-05-31 23:53:37 +0200

    First test

---
+++ careplan/patients_app/conftest.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User
+import pytest
+from django.forms import model_to_dict
+from django.test import Client
+from .models import *
+
+
+@pytest.fixture
+def user():
+    user = User.objects.create_user(id=1, username="user", password="")
+    return user
+
+
+@pytest.fixture
+def client():
+    new = Client()
+    return new
+
+
+@pytest.fixture
+def patients():
+    patient = Patients.objects.create(
+        first_name='Szymon',
+        last_name='Zietek',
+        year_of_birth='1994',
+        age='34',
+        pesel='15131612145',
+        address='Warszawa',
+        gender='Mężczyzna',
+        weight='100',
+        growth='178',
+        description_of_diseases='Opis chorób',
+        drugs_list='Lista leków'
+    )
+    return model_to_dict(patient)
--- careplan/patients_app/tests.py
+++ careplan/patients_app/tests.py
@@ -1,42 +1,61 @@
 from django.test import Client
-from django.test import TestCase
 from django.urls import reverse
-from django.contrib.messages import get_messages
-from .models import MedicalComponent
-from .forms import MedicalComponentForm
+from .models import Slider, Patients
+from .forms import PatientsForm
 import pytest
 
+
+@pytest.mark.django_db
 def test_main():
     client = Client()
     response = client.get('/')
     assert response.status_code == 200
 
 
-@pytest.mark.django_db
-def test_add_medical_component_success(self):
-    response = self.client.post(self.url, data=self.valid_data)
-    self.assertRedirects(response, reverse('first-page') + '?success=true')
-    self.assertEqual(response.status_code, 302)
-
-    # Check if the success message is displayed
-    messages = list(get_messages(response.wsgi_request))
-    self.assertEqual(len(messages), 1)
-    self.assertEqual(str(messages[0]), 'Dodano wyrób medyczny!')
-
-    # Check if the medical component is created in the database
-    self.assertEqual(MedicalComponent.objects.count(), 1)
-    medical_component = MedicalComponent.objects.first()
-    self.assertEqual(medical_component.name, 'Medical Component 1')
-    self.assertEqual(medical_component.description, 'Test description')
+def test_an_admin_view(admin_client):
+    response = admin_client.get("/admin/")
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_add_medical_component_invalid_data(self):
-    response = self.client.post(self.url, data=self.invalid_data)
-    self.assertEqual(response.status_code, 200)
+def test_first_site_view(client):
+    slider = Slider.objects.create(title='Slider 1', description='To jest nowa aplikacja', image='slider1.jpg')
+    response = client.get(reverse('first-page'))
+    assert response.status_code == 200
+    assert slider.title in str(response.content)
+    assert slider.description in str(response.content)
+    assert slider.image.url in str(response.content)
 
-    # Check if the form is displayed again with errors
-    self.assertTemplateUsed(response, 'your_template_name.html')
-    form = response.context['form']
-    self.assertIsInstance(form, MedicalComponentForm)
-    self.assertTrue(form.errors)
+
+@pytest.mark.django_db
+def test_add_patients_view(client):
+    response = client.get(reverse('add-patients'))
+    assert response.status_code == 200
+    assert isinstance(response.context['form'], PatientsForm)
+
+
+@pytest.mark.django_db
+def test_add_patients_view_post(client, patients):
+    response = client.post(reverse('add-patients'), data=patients, follow=True)
+    assert response.status_code == 200
+    assert Patients.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_patients_list_view(client, patients):
+    response = client.get(reverse('patients-list'))
+    assert response.status_code == 200
+    assert 'patients' in response.context
+    assert len(response.context['patients']) == Patients.objects.count()
+    assert 'search_query' in response.context
+    assert response.context['search_query'] == ''
+
+
+@pytest.mark.django_db
+def test_patients_list_view_search(client, patients):
+    response = client.get(reverse('patients-list'), {'search': '15131612145'})
+    assert response.status_code == 200
+    assert 'patients' in response.context
+    assert len(response.context['patients']) == 1
+    assert 'search_query' in response.context
+    assert response.context['search_query'] == '15131612145'
